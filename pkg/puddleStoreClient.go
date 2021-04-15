d25efaa6b91cb295aa808d45e21bffc9f3776d2a
John-J-Liu <zongjie_liu@brown.edu>
2021-04-15 16:00:04 -0400

    change some comment

--- pkg/puddleStoreClient.go
+++ pkg/puddleStoreClient.go
@@ -12,10 +12,10 @@ import (
 //may also need local map to provide concurrency
 
 type fileInfo struct {
-	// Filename   string
+	Filename string
 	Inode    *inode
 	Flush    bool
-	Modified []int
+	Modified map[int]string
 }
 
 type puddleStoreClient struct {
@@ -94,8 +94,9 @@ func (p *puddleStoreClient) Open(path string, create, write bool) (int, error) {
 			}
 			fd = p.getFd()
 			p.info[fd] = fileInfo{
-				Flush: write,
-				Inode: &node,
+				Filename: path,
+				Flush:    write,
+				Inode:    &node,
 			}
 			return fd, nil
 		}
@@ -111,7 +112,7 @@ func (p *puddleStoreClient) Open(path string, create, write bool) (int, error) {
 		//unlock
 		return fd, err
 	}
-	// todo when path is an dir, it should return err
+	// TODO when path is an dir, it should return err
 	if node.IsDir {
 		return fd, fmt.Errorf("it's a directory")
 	}
@@ -136,10 +137,18 @@ func (p *puddleStoreClient) Close(fd int) error {
 		//unlock
 		return nil
 	}
+	//update metadata in zookeeper, then unlcok
 	if len(info.Modified) != 0 {
-		//update metadata in zookeeper, then unlcok
-		//salt the GUID and publish it -> GUID_X1, GUID_X2
-		//need timeout to make sure at least one is published
+		for numBlock, guid := range info.Modified {
+			path := info.Filename + "-" + string(numBlock)
+			data := []byte(guid)
+			acl := zk.WorldACL(zk.PermAll)
+			_, err := p.Conn.Create(path, data, 0, acl)
+			if err != nil {
+				//unlock
+				return err
+			}
+		}
 	}
 	//clear fd
 	delete(p.info, fd)
@@ -163,7 +172,7 @@ func (p *puddleStoreClient) Read(fd int, offset, size uint64) ([]byte, error) {
 		//unlock
 		return []byte{}, fmt.Errorf("Get ChildrenW err")
 	}
-	//generate a random path from children
+	//TODO:generate a random path from children
 	child := children[0]
 	remote, err := p.getRandomRemote(child)
 	if err != nil {
@@ -184,26 +193,32 @@ func (p *puddleStoreClient) Read(fd int, offset, size uint64) ([]byte, error) {
 				}
 			}
 		}
-		//TODO: otherwise, request the data, only one is sufficient, how to get the data?
+		//TODO: otherwise, request the data, only one is sufficient?, how to get the data?
 	}
 	return nil, nil
 }
 
 func (p *puddleStoreClient) Write(fd int, offset uint64, data []byte) error {
 	//should return a copy of original data array and the client should be able to read its own write
+	//if anything fail, should we clear fd?
 	info, ok := p.info[fd]
 	if !ok {
 		return fmt.Errorf("invalid fd")
 	}
 	//Writing to An Open File With write=false should return errors
 	if !info.Flush {
+		//should we unlock?
 		return fmt.Errorf("write == false")
 	}
-	//if offset is greater than size, pad with 0
-	//In copy-on-write, you replace the blocks that the user writes to.
-	//fetch all the replica
+	if offset > info.Inode.Size {
+		//read and compare byte by byte?
+		//if offset is greater than size, pad with 0
+	}
+	//replace the blocks that the user writes to.
+	//salt the GUID and publish it -> GUID_X1, GUID_X2
+	//need timeout to make sure at least one is published
 	//Tapestry has no delete function, so you have all the different versions of a block in Tapestry
-	//but only the most recent version will be included in the iNode.
+	//but only the most recent version will be included in the iNode, do we need to fetch all the replica?
 	//cache the write
 	return nil
 }
@@ -223,6 +238,7 @@ func (p *puddleStoreClient) Remove(path string) error {
 	return nil
 }
 
+//is it recursive?
 func (p *puddleStoreClient) List(path string) ([]string, error) {
 	//TODO
 	return nil, nil
@@ -236,7 +252,6 @@ func (p *puddleStoreClient) Exit() {
 
 func (p *puddleStoreClient) getRandomRemote(path string) (*tapestry.RemoteNode, error) {
 	//load balancing: It's better to have client gets a random node each time when need to interact.
-	//TODO: get a random path
 	var remote tapestry.RemoteNode
 	id, err := p.getTapID(path)
 	if err != nil {
