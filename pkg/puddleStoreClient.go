9d6f0c6f55946b300c113ea8ca18ea0b42122678
John-J-Liu <zongjie_liu@brown.edu>
2021-04-17 16:20:02 -0400

    fileexist lock

--- pkg/puddleStoreClient.go
+++ pkg/puddleStoreClient.go
@@ -111,9 +111,9 @@ func (p *puddleStoreClient) unlock() {
 }
 
 func (p *puddleStoreClient) isFileExist(path string) (bool, error) {
-	// lock()
+	p.lock()
 	rlt, _, err := p.Conn.Exists(path)
-	//unlock()
+	p.unlock()
 	if err != nil {
 		return rlt, err
 	} else {
@@ -368,90 +368,6 @@ func (p *puddleStoreClient) Read(fd int, offset, size uint64) ([]byte, error) {
 	return rlt, nil
 }
 
-// func (p *puddleStoreClient) Read2(fd int, offset, size uint64) ([]byte, error) {
-// 	//should return a copy of original data array
-// 	// p.ClientMtx.Lock()
-// 	info, ok := p.info[fd]
-// 	// p.ClientMtx.Unlock()
-// 	if !ok {
-// 		return []byte{}, fmt.Errorf("invalid fd")
-// 	}
-// 	//handle edge case
-// 	if info.Inode.Size == 0 {
-// 		return []byte{}, nil
-// 	}
-// 	if offset > info.Inode.Size {
-// 		// return []byte{}, fmt.Errorf("reach here, %v", info.Inode.Size)
-// 		return []byte{}, nil
-// 	}
-
-// 	// data, err := p.readBlock(fd, 0)
-// 	//calculate the blocks we need to read
-// 	boundary := info.Inode.Size
-// 	if offset+size < boundary {
-// 		boundary = offset + size
-// 	} else {
-// 		size = boundary - offset
-// 	}
-
-// 	startBlock := offset / p.config.BlockSize
-// 	endBlock := boundary / p.config.BlockSize
-// 	offsetFirstBlock := offset % p.config.BlockSize
-// 	if startBlock == endBlock {
-// 		//first read from cache
-// 		data, err := p.readBlock2(fd, int(startBlock))
-// 		if len(data) == 0 || err != nil {
-// 			for i := 0; i < RETRY; i++ {
-// 				data, err = p.readBlock(fd, int(startBlock))
-// 				if err == nil && len(data) != 0 {
-// 					break
-// 				}
-// 			}
-// 			if err != nil || len(data) != 0 {
-// 				return []byte{}, err
-// 			}
-// 		}
-// 		// return nil, fmt.Errorf("%v, %v, %v, %v", data, offsetFirstBlock, size, data[offsetFirstBlock:(offsetFirstBlock+size)])
-// 		// return data[offsetFirstBlock:(offsetFirstBlock + size)], fmt.Errorf("reach here")
-// 		//cache it
-// 		return data[offsetFirstBlock:(offsetFirstBlock + size)], nil
-// 	}
-// 	var rlt []byte
-// 	var bytesRead uint64
-// 	bytesRead = 0
-// 	for i := startBlock; i <= endBlock; i++ {
-// 		//if cached, read locally
-// 		data, err := p.readBlock(fd, int(i))
-// 		if len(data) == 0 || err != nil {
-// 			for i := 0; i < RETRY; i++ {
-// 				data, err = p.readBlock(fd, int(startBlock))
-// 				if err == nil && len(data) != 0 {
-// 					break
-// 				}
-// 			}
-// 			if err != nil || len(data) != 0 {
-// 				return []byte{}, err
-// 			}
-// 		}
-// 		//cache it
-// 		if i == startBlock {
-// 			data = data[offsetFirstBlock:]
-// 			bytesRead = bytesRead + uint64(len(data)) - offsetFirstBlock
-// 		} else {
-// 			if i == endBlock {
-// 				left := size - bytesRead
-// 				if left < p.config.BlockSize {
-// 					data = data[:left]
-// 				}
-// 			} else {
-// 				bytesRead = bytesRead + uint64(len(data))
-// 			}
-// 		}
-// 		rlt = append(rlt, data...)
-// 	}
-// 	return rlt, nil
-// }
-
 func (p *puddleStoreClient) Write(fd int, offset uint64, data []byte) error {
 	//should return a copy of original data array and the client should be able to read its own write
 	//if anything fail, should we clear fd?
@@ -471,10 +387,6 @@ func (p *puddleStoreClient) Write(fd int, offset uint64, data []byte) error {
 	if len(data) == 0 {
 		return nil
 	}
-	//	 err := p.publish(fd, 0, data)
-	// if err != nil {
-	// 	return fmt.Errorf("problem in publish %v", err)
-	// }
 	// if offset > info.Inode.Size, [info.Inode.Size, offset) should be filled with 0
 	// write data []byte
 	// for each block, salt p.config.NumReplicas times and publish it
@@ -484,29 +396,6 @@ func (p *puddleStoreClient) Write(fd int, offset uint64, data []byte) error {
 	currentBlock := info.Inode.Size / p.config.BlockSize
 	startBlock := offset / p.config.BlockSize
 	endBlock := (offset + uint64(len(data))) / p.config.BlockSize
-	// if info.Inode.Size == 0 {
-	// 	if offset == 0{
-	// 		var size uint64 = 0
-	// 		for i := 0; i < int(endBlock); i++ {
-	// 			piece := data[:p.config.BlockSize]
-	// 			data = data[p.config.BlockSize:]
-	// 			p.savecache(fd, i, piece)
-	// 			size += p.config.BlockSize
-	// 		}
-	// 		piece := make([]byte, p.config.BlockSize)
-	// 		for i, v := range data {
-	// 			piece[i] = v
-	// 		}
-	// 		p.savecache(fd, int(endBlock), piece)
-	// 		size = size + uint64(len(data))
-	// 		for i := 0; i <= int(endBlock); i++ {
-	// 			p.info[fd].Modified[i] = true
-	// 		}
-	// 		p.info[fd].Inode.Size += uint64(size)
-	// 		return nil
-	// 	}
-	// }
-
 	if offset > info.Inode.Size {
 		// if offset > info.Inode.Size, [info.Inode.Size, offset) should be filled with 0
 		if info.Inode.Size%p.config.BlockSize == 0 {
@@ -582,150 +471,11 @@ func (p *puddleStoreClient) Write(fd int, offset uint64, data []byte) error {
 		r = append(r, tmp[len(data):]...)
 		p.savecache(fd, int(endBlock), r)
 	}
-	// return fmt.Errorf("%v, %v, %v", offset, len(data), p.info[fd].Inode.Size)
 	if offset+uint64(dataLen) > p.info[fd].Inode.Size {
 		p.info[fd].Inode.Size = offset + uint64(dataLen)
 	}
-	// return fmt.Errorf("%v, %v, %v", offset, len(data), p.info[fd].Inode.Size)
 	return nil
 }
-
-// func (p *puddleStoreClient) Write2(fd int, offset uint64, data []byte) error {
-// 	//should return a copy of original data array and the client should be able to read its own write
-// 	//if anything fail, should we clear fd?
-// 	// p.ClientMtx.Lock()
-// 	// return fmt.Errorf("%v, %v, %v, %v", len(data), offset, p.config.BlockSize, data)
-// 	info, ok := p.info[fd]
-// 	// p.ClientMtx.Unlock()
-// 	if !ok {
-// 		return fmt.Errorf("invalid fd")
-// 	}
-// 	//Writing to An Open File With write=false should return errors
-// 	if !info.Flush {
-// 		//should we unlock?
-// 		return fmt.Errorf("write == false")
-// 	}
-// 	//handle edge case
-// 	if len(data) == 0 {
-// 		return nil
-// 	}
-// 	// err := p.publish(fd, 0, data)
-// 	// if err != nil {
-// 	// 	return fmt.Errorf("problem in publish %v", err)
-// 	// }
-// 	// if offset > info.Inode.Size, [info.Inode.Size, offset) should be filled with 0
-// 	// write data []byte
-// 	// for each block, salt p.config.NumReplicas times and publish it
-// 	// make sure at least one is published
-// 	// cache the write
-// 	currentBlock := info.Inode.Size / p.config.BlockSize
-// 	startBlock := offset / p.config.BlockSize
-// 	endBlock := (offset + uint64(len(data))) / p.config.BlockSize
-// 	// if info.Inode.Size == 0 {
-// 	// 	if offset == 0{
-// 	// 		var size uint64 = 0
-// 	// 		for i := 0; i < int(endBlock); i++ {
-// 	// 			piece := data[:p.config.BlockSize]
-// 	// 			data = data[p.config.BlockSize:]
-// 	// 			p.savecache(fd, i, piece)
-// 	// 			size += p.config.BlockSize
-// 	// 		}
-// 	// 		piece := make([]byte, p.config.BlockSize)
-// 	// 		for i, v := range data {
-// 	// 			piece[i] = v
-// 	// 		}
-// 	// 		p.savecache(fd, int(endBlock), piece)
-// 	// 		size = size + uint64(len(data))
-// 	// 		for i := 0; i <= int(endBlock); i++ {
-// 	// 			p.info[fd].Modified[i] = true
-// 	// 		}
-// 	// 		p.info[fd].Inode.Size += uint64(size)
-// 	// 		return nil
-// 	// 	}
-// 	// }
-
-// 	if offset > info.Inode.Size {
-// 		// if offset > info.Inode.Size, [info.Inode.Size, offset) should be filled with 0
-// 		if info.Inode.Size%p.config.BlockSize == 0 {
-// 			p.savecache(fd, int(currentBlock), make([]byte, p.config.BlockSize))
-// 		}
-// 		for startBlock > currentBlock {
-// 			currentBlock += 1
-// 			p.savecache(fd, int(currentBlock), make([]byte, p.config.BlockSize))
-// 		}
-// 	}
-// 	if startBlock == endBlock {
-// 		tmp, err := p.readBlock(fd, int(startBlock))
-// 		if err != nil {
-// 			for i := 0; i < RETRY; i++ {
-// 				tmp, err = p.readBlock(fd, int(startBlock))
-// 				if err == nil {
-// 					break
-// 				}
-// 			}
-// 		}
-// 		if err != nil {
-// 			return err
-// 		}
-// 		if len(tmp) == 0 {
-// 			tmp = make([]byte, p.config.BlockSize)
-// 		}
-// 		r := tmp[:offset%p.config.BlockSize]
-// 		r = append(r, data...)
-// 		r = append(r, tmp[(offset+uint64(len(data)))%p.config.BlockSize:]...)
-// 		p.savecache(fd, int(startBlock), r)
-// 		return fmt.Errorf("%v", r)
-// 	} else {
-// 		tmp, err := p.readBlock(fd, int(startBlock))
-// 		if err != nil {
-// 			for i := 0; i < RETRY; i++ {
-// 				tmp, err = p.readBlock(fd, int(startBlock))
-// 				if err == nil {
-// 					break
-// 				}
-// 			}
-// 		}
-// 		if err != nil {
-// 			return err
-// 		}
-// 		if len(tmp) == 0 {
-// 			tmp = make([]byte, p.config.BlockSize)
-// 		}
-// 		r := tmp[:offset%p.config.BlockSize]
-// 		initbytes := p.config.BlockSize - offset%p.config.BlockSize
-// 		r = append(r, data[:initbytes]...)
-// 		p.savecache(fd, int(startBlock), r)
-// 		data = data[initbytes:]
-// 		for i := startBlock + 1; i < endBlock; i++ {
-// 			r = data[:p.config.BlockSize]
-// 			data = data[p.config.BlockSize:]
-// 			p.savecache(fd, int(i), r)
-// 		}
-// 		tmp, err = p.readBlock(fd, int(endBlock))
-// 		if err != nil {
-// 			for i := 0; i < RETRY; i++ {
-// 				tmp, err = p.readBlock(fd, int(startBlock))
-// 				if err == nil {
-// 					break
-// 				}
-// 			}
-// 		}
-// 		if err != nil {
-// 			return err
-// 		}
-// 		if len(tmp) == 0 {
-// 			tmp = make([]byte, p.config.BlockSize)
-// 		}
-// 		r = data[:]
-// 		r = append(r, tmp[len(data):]...)
-// 		p.savecache(fd, int(endBlock), r)
-// 	}
-// 	if offset+uint64(len(data)) > p.info[fd].Inode.Size {
-// 		p.info[fd].Inode.Size = offset + uint64(len(data))
-// 	}
-// 	// return fmt.Errorf("%v, %v, %v", offset, len(data), p.info[fd].Inode.Size)
-// 	return nil
-// }
 
 func (p *puddleStoreClient) savecache(fd, numBlock int, data []byte) {
 	p.ClientMtx.Lock()
